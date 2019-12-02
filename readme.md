054eb58b0a28a6d1f519f665ae8a0964e06f6c38
최영규 <choiyeong-gyu@ygtoken.local>
2019-12-02 17:57:23 +0900

    test typescript function hello2 추가

--- readme.md
+++ readme.md
@@ -117,3 +117,5 @@ Serverless 프레임워크를 기반으로 Typescript & Mocha를 적용한 AWS D
 - https://www.jamestharpe.com/serverless-typescript-getting-started/
 - https://www.jeremydaly.com/how-to-add-test-coverage-to-your-serverless-applications/
 - https://medium.com/@jwyeom63/%EB%B9%A0%EB%A5%B4%EA%B2%8C-%EB%B0%B0%EC%9B%8C%EB%B3%B4%EB%8A%94-node-js%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EC%84%9C%EB%B2%84%EB%A6%AC%EC%8A%A4-serverless-503ee61539d4
+- https://blog.shovonhasan.com/deploying-a-typescript-node-aws-lambda-function-with-serverless/
+- https://scotch.io/@nwayve/how-to-build-a-lambda-function-in-typescript
--- src/functions/handler.ts
+++ src/functions/handler.ts
@@ -1,7 +1,12 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { APIGatewayProxyHandler, Handler, Context, Callback } from "aws-lambda";
 import "source-map-support/register";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+interface HelloResponse {
+  statusCode: number;
+  body: string;
+}
+
+const hello: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -15,3 +20,15 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
     )
   };
 };
+
+const hello2: Handler = (event: any, context: Context, callback: Callback) => {
+  const response: HelloResponse = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: Math.floor(Math.random() * 10)
+    })
+  };
+  callback(undefined, response);
+};
+
+export { hello, hello2 };
